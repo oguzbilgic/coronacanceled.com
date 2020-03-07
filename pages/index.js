dd9835d9f72abdc3cedc5c1ecb459acdabf98aad
Oguz Bilgic <fisyonet@gmail.com>
2020-03-07 01:04:01 -0800

    Add index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <div>
+      <h2>Events Canceled, Postponed or In-person only due to Corona Virus</h2>
+
+      <ul>
+        <li>
+          <a href="https://www.sxsw.com/2020-event-update/">
+            SXSW Canceled
+          </a> March 6-12, 2020 in Austin, Texas
+        </li>
+
+        <li>
+          <a href="https://summit.adobe.com/na/">
+            Adobe Summit - Online only
+          </a> March 6-April 2, 2020 in Las Vegas
+        </li>
+
+        <li>
+          <a href="https://www.arubanetworks.com/atmosphere/">
+            Aruba Networks Atmosphere 2020 - Online only
+          </a> March 23-26, 2020 in Austin Texas
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default HomePage

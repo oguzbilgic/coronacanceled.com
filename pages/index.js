0ef9df3c3cc1118f99e42526b83f5944ddd542bd
Oguz Bilgic <fisyonet@gmail.com>
2020-03-07 01:23:36 -0800

    Add events

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,56 @@
+const events = [
+  {
+    name: "Facebook F8",
+    link: "https://www.f8.com/",
+    location: "San Jose, California",
+    date: "May 6-8, 2020",
+    status: "canceled"
+  },
+  {
+    name: "SXSW 2020",
+    link: "https://www.sxsw.com/",
+    location: "Austin, Texas",
+    date: "March 6-12, 2020",
+    status: "canceled"
+  },
+  {
+    name: "Adobe Summit",
+    link: "https://summit.adobe.com/na/",
+    location: "Las Vegas",
+    date: "March 29 - April 2, 2020",
+    status: "online-only"
+  },
+  {
+    name: "Cisco Live Melbourne",
+    link: "https://www.ciscolive.com/apjc.html",
+    location: "Melbourne",
+    date: "March 3-6, 2020",
+    status: "canceled"
+  },
+  {
+    name: "Facebook Global Marketing Summit",
+    link: "",
+    location: "San Francisco, California",
+    date: "March 9-12, 2020",
+    status: "canceled"
+  },
+]
+
 function HomePage() {
   return (
     <div>
-      <h2>Events Canceled, Postponed or In-person only due to Corona Virus</h2>
+      <h2>Events Canceled, Postponed or Online-only due to Corona Virus</h2>
 
       <ul>
-        <li>
-          <a href="https://www.sxsw.com/2020-event-update/">
-            SXSW Canceled
-          </a> March 6-12, 2020 in Austin, Texas
-        </li>
-
-        <li>
-          <a href="https://summit.adobe.com/na/">
-            Adobe Summit - Online only
-          </a> March 6-April 2, 2020 in Las Vegas
-        </li>
-
-        <li>
-          <a href="https://www.arubanetworks.com/atmosphere/">
-            Aruba Networks Atmosphere 2020 - Online only
-          </a> March 23-26, 2020 in Austin Texas
-        </li>
+        {events.map(event => 
+          <li>
+            <a href={event.link}>
+              {event.name}
+            </a> <b>{event.status}</b> {event.date} in {event.location}
+          </li>
+        )}
       </ul>
+      <a>Update on github</a>
     </div>
   )
 }

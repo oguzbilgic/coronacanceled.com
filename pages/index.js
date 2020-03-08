b2c9b01637282a1b325106ab9ca517ba3a0d88eb
Oguz Bilgic <fisyonet@gmail.com>
2020-03-08 04:11:30 -0700

    Add sports

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ const contributors = [
 const events = [
   {
     name: "Facebook F8",
+    category: 'tech',
     link: "https://www.f8.com/",
     location: "San Jose, California",
     date: "May 6-8, 2020",
@@ -17,6 +18,7 @@ const events = [
   },
   {
     name: "SXSW 2020",
+    category: 'tech',
     link: "https://www.sxsw.com/",
     location: "Austin, Texas",
     date: "March 6-12, 2020",
@@ -24,6 +26,7 @@ const events = [
   },
   {
     name: "Cisco Live Melbourne",
+    category: 'tech',
     link: "https://www.ciscolive.com/apjc.html",
     location: "Melbourne",
     date: "March 3-6, 2020",
@@ -31,6 +34,7 @@ const events = [
   },
   {
     name: "Facebook Global Marketing Summit",
+    category: 'tech',
     link: "",
     location: "San Francisco, California",
     date: "March 9-12, 2020",
@@ -38,6 +42,7 @@ const events = [
   },
   {
     name: "Google I/O",
+    category: 'tech',
     link: "https://events.google.com/io/",
     location: "Mountain View, California",
     date: "May 12-14, 2020",
@@ -45,6 +50,7 @@ const events = [
   },
   {
     name: "SAP Ariba LIVE",
+    category: 'tech',
     link: "https://fusion.concur.com/fusion-orlando-announcement",
     location: "Las Vegas",
     date: "March 16-18, 2020",
@@ -52,6 +58,7 @@ const events = [
   },
   {
     name: "Adobe Summit",
+    category: 'tech',
     link: "https://summit.adobe.com/na/",
     location: "Las Vegas",
     date: "March 29 - April 2, 2020",
@@ -59,6 +66,7 @@ const events = [
   },
   {
     name: "Google Cloud Next",
+    category: 'tech',
     link: "https://cloud.withgoogle.com/next/sf",
     location: "San Francisco, California",
     date: "April 6-8, 2020",
@@ -66,6 +74,7 @@ const events = [
   },
   {
     name: "SAS Global Forum",
+    category: 'tech',
     link: "https://www.sas.com/en_us/events/sas-global-forum.html",
     location: "Washington, DC",
     date: "March 29 - April 1, 2020",
@@ -73,6 +82,7 @@ const events = [
   },
   {
     name: "Shopify Unite 2020 Dev Conference",
+    category: 'tech',
     link: "https://unite.shopify.com/",
     location: "Toronto",
     date: "May 6-8, 2020",
@@ -80,6 +90,7 @@ const events = [
   },
   {
     name: "Atlassian Summit 2020",
+    category: 'tech',
     link: "https://www.atlassian.com/company/events/summit",
     location: "Las Vegas, Nevada",
     date: "March 31 - April 2, 2020",
@@ -87,6 +98,7 @@ const events = [
   },
   {
     name: "DocuSign Momentum",
+    category: 'tech',
     link: "https://momentum.docusign.com/",
     location: "San Francisco, California",
     date: "March 4, 2020",
@@ -94,6 +106,7 @@ const events = [
   },
   {
     name: "dotGo 2020",
+    category: 'tech',
     link: "https://www.dotgo.eu/",
     location: "Paris, France",
     date: "March 30, 2020",
@@ -101,6 +114,7 @@ const events = [
   },
   {
     name: "ESRI Developer Summit",
+    category: 'tech',
     link: "https://www.esri.com/en-us/about/events/devsummit/overview",
     location: "Palm Springs, California",
     date: "March 10, 2020",
@@ -108,6 +122,7 @@ const events = [
   },
   {
     name: "Microsoft WSLConf",
+    category: 'tech',
     link: "https://www.wslconf.dev/",
     location: "Redmond, Washington",
     date: "March 10-11, 2020",
@@ -115,6 +130,7 @@ const events = [
   },
   {
     name: "Black Hat Asia 2020",
+    category: 'tech',
     link: "https://www.blackhat.com/asia-20/",
     location: "Singapore",
     date: "March 31 - April 3, 2020",
@@ -122,6 +138,7 @@ const events = [
   },
   {
     name: "EmTech Asia",
+    category: 'tech',
     link: "https://emtechasia.com/",
     location: "Singapore",
     date: "March 24, 2020",
@@ -129,6 +146,7 @@ const events = [
   },
   {
     name: "Game Developers Conference (GDC)",
+    category: 'tech',
     link: "https://www.gdconf.com/",
     location: "San Francisco, California",
     date: "March 16-20, 2020",
@@ -136,6 +154,7 @@ const events = [
   },
   {
     name: "Kaspersky's Security Analyst Summit",
+    category: 'tech',
     link: "https://thesascon.com/",
     location: "Barcelona",
     date: "April 6-9, 2020",
@@ -143,6 +162,7 @@ const events = [
   },
   {
     name: "Paris Blockchain Week Summit",
+    category: 'tech',
     link: "https://www.pbwsummit.com/",
     location: "Paris, France",
     date: "March 31 - April 1, 2020",
@@ -150,6 +170,7 @@ const events = [
   },
   {
     name: "F5 Agility 2020",
+    category: 'tech',
     link: "https://www.f5.com/agility",
     location: "Orlando, Florida",
     date: "March 16-19, 2020",
@@ -157,6 +178,7 @@ const events = [
   },
   {
     name: "Ingram Micro Cloud Cloud Summit 2020 ",
+    category: 'tech',
     link: "https://www.ingrammicrocloud.com/press-releases/ingram-micro-cloud-postpones-cloud-summit-2020-early-2021/",
     location: "Miami Beach, Florida",
     date: "March 12-14, 2020",
@@ -164,6 +186,7 @@ const events = [
   },
   {
     name: "Microsoft MVP Global Summit",
+    category: 'tech',
     link: "https://mvp.microsoft.com/summit",
     location: "Redmond, Washington",
     date: "March 15-20, 2020",
@@ -171,12 +194,65 @@ const events = [
   },
   {
     name: "First Robotics Mt Olive NJ",
+    category: 'tech',
     link: "https://midatlanticrobotics.com/news/2020-mount-olive-district-event-cancellation/",
     location: "Mt Olive, New Jersey",
     date: "March 7-8, 2020",
     status: "canceled"
-  }
+  },
+  {
+    name: "Formula 1: The Bahrain Grand Prix ",
+    category: 'sports',
+    link: "",
+    location: "Bahrain",
+    date: "March 20-22, 2020",
+    status: "online-only"
+  },
+  {
+    name: "Barcelona Marathon",
+    category: 'sports',
+    link: "",
+    location: "Barcelona, Spain",
+    date: "March 15, 2020",
+    status: "postponed"
+  },
+  {
+    name: "2020 IIHF Women's World Championship",
+    category: 'sports',
+    link: "",
+    location: "Canada",
+    date: "March 31 - April 10, 2020",
+    status: "canceled"
+  },
+  {
+    name: "World Athletics Half Marathon Championships",
+    category: 'sports',
+    link: "",
+    location: "Gdynia, Poland",
+    date: "March 29, 2020",
+    status: "postponed"
+  },
+  {
+    name: "Formula E: The Rome E-Prix",
+    category: 'sports',
+    link: "",
+    location: "Rome, Italy",
+    date: "April 4, 2020",
+    status: "postponed"
+  },
 ]
+
+const EventsList = ({ events }) => (
+  <ul>
+    {events.map(event => 
+    <li>
+      <a href={event.link}>
+        {event.name}
+      </a> <b>{event.status}</b> {event.date} in {event.location}
+    </li>
+    )}
+  </ul>
+)
 
 function HomePage() {
   useEffect(() => {
@@ -199,10 +275,10 @@ function HomePage() {
         <a href="https://twitter.com/intent/tweet?text=event+info+https://coronacanceled.com">Tweet to add event info</a>
       </div>
 
-      <h2>Canceled Events Due to Corona Virus - COVID-19</h2>
+      <h2>Effected Sports Events Due to Corona Virus - COVID-19</h2>
 
       <ul>
-        {events.filter(e => e.status == 'canceled').map(event => 
+        {events.filter(e => e.category == 'sports').map(event => 
           <li>
             <a href={event.link}>
               {event.name}
@@ -211,22 +287,10 @@ function HomePage() {
         )}
       </ul>
 
-      <h2>Online Only Events Due to Corona Virus - COVID-19</h2>
+      <h2>Effected Tech Events Due to Corona Virus - COVID-19</h2>
 
       <ul>
-        {events.filter(e => e.status == 'online-only').map(event => 
-          <li>
-            <a href={event.link}>
-              {event.name}
-            </a> <b>{event.status}</b> {event.date} in {event.location}
-          </li>
-        )}
-      </ul>
-
-      <h2>Postponed Events Due to Corona Virus - COVID-19</h2>
-
-      <ul>
-        {events.filter(e => e.status == 'postponed').map(event => 
+        {events.filter(e => e.category == 'tech').map(event => 
           <li>
             <a href={event.link}>
               {event.name}
